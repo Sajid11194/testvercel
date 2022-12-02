90369ddc5f1c93f4240bdea203a23403b4c79c28
sajid <sajid@example.com>
2022-12-03 03:19:19 +0600

    a

---
+++ app.js
@@ -0,0 +1,20 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.listen(80, () => {
+  console.log("Server Started");
+});
+app.get("/",(req,res)=>{
+  res.json("Hi")
+})
